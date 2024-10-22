218a1514bc7139979b10439bce800afdf04c8676
Asora7 <verstr01474@stud.noroff.no>
2024-10-22 15:04:56 +0200

    updating package.json

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@
  * @property {Object} transform - The transformation settings for file types.
  * @property {string} transform- ['^.+\\.jsx?$'] - The regex pattern to match JavaScript files for transformation.
  * @property {string} setupFiles - The path to the setup file that will run before tests.
+ * @property {Array<string>} testPathIgnorePatterns - Array of paths that Jest should ignore when running tests.
  */
 
 export default {
@@ -21,4 +22,5 @@ export default {
     '^.+\\.jsx?$': 'babel-jest', // Transforms JavaScript and JSX files using Babel
   },
   setupFiles: ['./jest.setup.js'], // Path to the setup file
+  testPathIgnorePatterns: ['/node_modules/', '/cypress/'], // Ignore Cypress test files
 };
